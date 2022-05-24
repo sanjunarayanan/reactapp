229d63788afbb90d287e758b92a4db16562d231f
sanjunarayanan <sanjunarayanan123@gmail.com>
2022-05-24 16:25:12 +0530

    front end with react router version 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import {Container} from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {Container} from 'react-bootstrap'
+import HomeScreen  from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const App=()=> {
   return (
     <>
+    <Router>
     <Header></Header>
     <main className='py-3'>
       <Container>
-      <h1>Welocome to the proshop</h1>
+         <Routes> 
+          <Route path='/' element={<HomeScreen></HomeScreen>} exact />
+          <Route path='/product/:id' element={<ProductScreen></ProductScreen>} />
+        </Routes>
       </Container>
     </main>
     <Footer></Footer>
+    </Router>
     </>
-    
   );
 }
 
