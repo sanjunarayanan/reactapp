b5321c986ee0f562bd3b9d28f7691e612ec24058
sanjunarayanan <sanjunarayanan123@gmail.com>
2022-06-04 13:05:17 +0530

    error handling , mongodb set up

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const router = express.Router()
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+
+router.get('/',asyncHandler(async(req,res)=>{
+  const products = await Product.find({})
+  res.json(products)
+}))
+
+
+router.get('/:id',asyncHandler(async(req,res)=>{
+  const product = await Product.findById(req.params.id)
+  if(product){
+    res.json(product)
+  }else {
+    throw new Error('product not found..')
+  }
+}))
+
+export default router
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import {errorHandler,notFound} from './middleware/errorMiddleware.js'
+
+dotenv.config();
+const app = express()
+const PORT = process.env.PORT || 5000
+
+connectDB()
+
+
+app.get('/',(req,res)=>{
+    res.send('app is running')
+})
+
+
+
+app.use('/api/products', productRoutes)
+app.use(notFound);
+app.use(errorHandler)
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
+
